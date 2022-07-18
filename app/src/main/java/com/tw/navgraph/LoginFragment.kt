d2d4bdcce19e9d847a31c60a59438a9513a19fea
Nandhagobal A <nandhagobal@Nandhagobals-MacBook-Pro.local>
2022-07-18 10:42:59 +0530

    Add functionality of receiving argument from login fragment to welcome fragment

--- app/src/main/java/com/tw/navgraph/LoginFragment.kt
+++ app/src/main/java/com/tw/navgraph/LoginFragment.kt
@@ -3,17 +3,21 @@ package com.tw.navgraph
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 class LoginFragment :Fragment(R.layout.fragment_login) {
     lateinit var loginButton : Button
+    lateinit var userNameEV : EditText
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         loginButton = view.findViewById(R.id.loginButton)
+        userNameEV = view.findViewById(R.id.userNameEV)
 
         loginButton.setOnClickListener{
-            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeScreenFragment()
+            val userName = userNameEV.text.toString()
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeScreenFragment(userName)
             findNavController().navigate(action)
         }
     }
--- app/src/main/java/com/tw/navgraph/WelcomeScreenFragment.kt
+++ app/src/main/java/com/tw/navgraph/WelcomeScreenFragment.kt
@@ -3,14 +3,20 @@ package com.tw.navgraph
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 class WelcomeScreenFragment : Fragment(R.layout.fragment_welcome_screen) {
     lateinit var goBackToHomeButton :Button
+    lateinit var userNameTV : TextView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         goBackToHomeButton = view.findViewById(R.id.backToHome)
+        val userName = arguments?.get("userName") as String
+        userNameTV = view.findViewById(R.id.userNameTV)
+        userNameTV.text = userName
+
          goBackToHomeButton.setOnClickListener{
              val action = WelcomeScreenFragmentDirections.actionWelcomeScreenFragmentToHomeFragment()
              findNavController().navigate(action)
